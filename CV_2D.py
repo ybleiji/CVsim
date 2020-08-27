Correct the normalization problem
The equations for the 2D and 3D were not right. The equations are now corrected.

CV_2D.py
@@ -60,6 +60,10 @@ def CV_2D(self, *args, **kwargs):
     
     --------------------------------------------------------------------------
     REMARKS:   
+    The zeroth entry are the y positions and the first elements are the x 
+    positions. Such that if one makes a 2D plot the y directions is along the
+    vertical axis and the x direction is along the horizontal axis.
+        
     In order to have a stable calculation, the maximum value of the diffusion
     model coefficient is:
         Dmx + Dmy < 0.5, Dmi = D dt/(di)^2
@@ -72,9 +76,11 @@ def CV_2D(self, *args, **kwargs):
     
     If one wants to use different diffusion constants, a problem can occur.
     If D1 >> D2 or visa versa, the large D requires a large simulation length.
-    Keeping the amount of x_steps constant for both D, than the dx will be way
+    When keeping the amount of x_steps constant for both D, the dx will be way
     too large for the species with the small D. Therefore, the dx is assigned
-    to the species individually.
+    to the species individually. When a non flate electrode is used, a single
+    value of dx and dy will be choosen such that the electrode is at the same
+    spatial positions for both species.
     '''
     # initialize parameters
     E0, E1, E2, Ef = self.E0, self.E1, self.E2, self.Ef # all in V
@@ -126,36 +132,28 @@ def CV_2D(self, *args, **kwargs):
         electrode[:,0] = 1
         self.elec = electrode
 
-
     #check if a blocking layer is present
-    if 'block' in dir(self):
-        # use findsurface to find the elements and directions of the surface of the blocking layer
-        block_surf, blocked, empty, normal, block_surf0, blocked0, normal0 = self.findsurface(self.block)
-        self.block_surf, self.blocked = block_surf, blocked
-        elec_block = electrode+2*self.block
+    if 'block' in dir(self): # use findsurface to find the elements and directions of the surface of the blocking layer
+        blocked, normal_block  = self.findsurface(self.block)
+        blocked_surf = blocked + normal_block
+        self.blocked = blocked
+        elec_block = electrode + 2 * self.block
         electrode = np.where(elec_block==1,1,0) # remove part of electrode that is blocked
         electrolyte = np.where(np.logical_or(electrode,self.block)==0,1,0)
-        blockpres, lenblock = True, len(blocked) # blocking layer is present, set the length
-    else:
-        # where is the electrolyte and check if list elec is not empty
+        blockpres = True # blocking layer is present
+    else: # where is the electrolyte and check if list elec0 is not empty
         electrolyte = np.where(electrode==0,1,0)
         blockpres = False # blocking layer is not present
     
     # use findsurface to find the elements and directions of the surface
-    el_surf, elec, empty, normal, el_surf0, elec0, normal0 = self.findsurface(electrode)
-    # el_surf, elec, empty, normal = self.findsurface(electrode)
-    self.el, self.el_surf = elec, el_surf
-    # el_surf is a list containing the positions of the surface of the electrode (x=dx)
-    # elec is a list containing the positions of the electrode (x=0)
+    elec, normal = self.findsurface(electrode)
+    self.el, self.normal = elec, normal
+    # elec is an 2xN array containing all unique surface elements
+    # normal is an 2xN array containing all the normal vectors to the surface 
     
-    # allow the ions to difusse into the surface of the elec/blocking layer (needed for BC)
-    # electrolyte[tuple(elec0)] = 1
-    for el in elec:
-        electrolyte[tuple(el)] = 1
-    # if blockpres: electrolyte[tuple(blocked0)] = 1
-    if blockpres: 
-        for el in blocked:
-            electrolyte[tuple(el)] = 1
+    # allow the ions to difusse into the surface of the elec0/blocking layer (needed for BC)
+    electrolyte[tuple(elec)] = 1
+    if blockpres: electrolyte[tuple(blocked)] = 1
             
     # create the mesh and _profiles
     meshA = np.meshgrid(xvalA,yvalA)
@@ -167,54 +165,31 @@ def CV_2D(self, *args, **kwargs):
     beta = 1-alpha # alpha + beta = 1
     
     #initize some parameters for the diffusion
-    dx, dy = [abs(el[:,1]-el[:,0]) for el in [xval, yval]] # in cm
+    dx, dy = [abs(el[:,1]-el[:,0]) for el in [xval, yval]] # in cm: dx[0] species A, dx[1] species B
     dt = min(self.Dm/D * (dx*dy)**2/(dx**2+dy**2)) # in s (select the smallest of the two)
     Dm_x, Dm_y = [np.reshape(D*dt/el**2, (2,1,1)) for el in [dx,dy]]
-    diff = np.delete([dx,dx,dy,dy],empty, axis=0) # list with differences corresponding to non empty elements
-    dD = [(el/D*np.array([1,-1])).reshape((2,1)) for el in diff] # speeds up the simulation, [1,-1] is for positive and negative flux
     self.dt = dt
+
+    # find the surface elements by displacing the electrode element with the normal vector
+    el_surfx = np.array([elec[0], elec[1] + normal[1]]) 
+    el_surfy = np.array([elec[0] + normal[0], elec[1]])  
     
+    # normalize the normal vector
+    norm_dx_dy = normal * np.array([[dy[0]],[dx[0]]])
+    normal = abs(norm_dx_dy/(np.sqrt(norm_dx_dy[0]**2 + norm_dx_dy[1]**2)))
     
+    # some constants to speed up the simulation
+    # normal[1] = nx, normal[0] = ny
+    dx, dy  = dx.reshape((2,1)), dy.reshape((2,1))
+    denom = normal[1] * dy + normal[0] * dx
+    A, B = normal[1] * dy / denom, normal[0] * dx / denom
+    C = dx * dy / (D.reshape((2,1)) * denom)
     
-    
-    
-    
-    # normalize the normal vector using the dx and dy values
-    x_or_y_dir = np.delete(np.array([0,0,1,1]), empty) # zeroth entry is x, first entry is y
-    for idx, el in enumerate(normal):
-        temp = abs(el) * np.array([[dx[0]],[dy[0]]])
-        normal[idx] = (temp/(np.sqrt(temp[0]**2 + temp[1]**2)))[x_or_y_dir[idx]]
-        
-    # temp = normal0 * np.array([[dx[0]],[dy[0]]])
-    # normal0 = (temp/(np.sqrt(temp[0]**2 + temp[1]**2)))
-    
-    
-    
-    # return
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # create time and Eapp array using dt
     dEmax = dt*scanrate # max dE in V
     Npoints = int(( max(E0,E1,E2,Ef)-min(E0,E1,E2,Ef) )/ dEmax + 0.5 ) # round up
     Eapp_r,time_r = self.E_app(E0,E1,E2,scanrate,Ef,ncycles,Npoints) 
     dt  = abs(time_r[1]-time_r[0]) # new dt in s, which is smaller the the old dt
 
-    #warn if the length of time is large!
-    if len(time_r) > 50000:
-        print('The simulation might take a while since {} itterations are required!'.format(len(time_r)))
-    if len(time_r) > self.MaxIter:
-        raise RecursionError('The simulation needs more than {} iterations! The simulation is terminated!'.format(self.MaxIter))
-    
     # create Eapp and time array for the output
     E_step = round(Npoints/E_steps)
     E_step = E_step if E_step > 0 else 1
@@ -227,18 +202,22 @@ def CV_2D(self, *args, **kwargs):
     # assume Ef0 stays constant
     k_c = self.ECrateconst(Eapp_r,Ef0,k0[0],T_C,alpha)
     k_a = self.ECrateconst(Eapp_r,Ef0,k0[1],T_C,-beta)
-    k = np.array([k_c,k_a]) # in cm/s, c: reduction and a: oxidation
     
     # make dictionary to speed up the simulation 
     c_cache = {}
-    c_cache[0] = c_profile
-    c_prev = c_profile
+    c_cache[0], c_prev = c_profile, c_profile
     
     try: fbN = self.fbN
     except AttributeError: fbN = int(round(len(time_r)/11))
     infobool = False
+    # check if N0 of iter < MaxIter
+    if len(time_r) > self.MaxIter:
+        raise RecursionError('The simulation needs more than {} iterations! The simulation is terminated!'.format(self.MaxIter))
+    # give some feedback    
     if self.feedback: 
-        print('Amount of iterations required: {}\n'.format(len(time_r)))
+        #warn if the length of time is large!
+        if len(time_r) > 50000: print('The simulation might take a while since {} itterations are required!'.format(len(time_r)))
+        else: print('Amount of iterations required: {}\n'.format(len(time_r)))
         if len(time_r) >  fbN:
             infobool = True
             print('################################################')
@@ -247,16 +226,16 @@ def CV_2D(self, *args, **kwargs):
     
     infosec = '# {:>6}| {:>7}| {:>10.4} sec| {:>8.5} sec #'
     infomin = '# {:>6}| {:>7}| {:>10.4} min| {:>8.5} sec #'
-    counter_t = 1
-    t0 = time.perf_counter()
     
     # start time of the sim
+    counter_t, t0 = 1, time.perf_counter()
     for idx,t in enumerate(time_r[1:],1):
         if idx % fbN == 0 and infobool: # some feedback
             if idx == fbN: t1 = time.perf_counter()
             tleft = (len(time_r)-idx)*(t1-t0)/fbN
             if tleft > 120: print(infomin.format(idx,len(time_r)-idx, (len(time_r)-idx)*(t1-t0)/fbN/60, time.perf_counter()-t0))
             else: print(infosec.format(idx,len(time_r)-idx, (len(time_r)-idx)*(t1-t0)/fbN, time.perf_counter()-t0))
+        # diffusion part
         cnewx = Dm_x*( np.roll(c_prev,-1, axis=2) - 2*c_prev + np.roll(c_prev,1,axis=2) )
         cnewy = Dm_y*( np.roll(c_prev,-1, axis=1) - 2*c_prev + np.roll(c_prev,1,axis=1) )
         c_new = (c_prev + cnewx + cnewy) * electrolyte # non zero concentrations where electrolyte = 1
@@ -264,83 +243,44 @@ def CV_2D(self, *args, **kwargs):
         c_new[:,[0,-1]], c_new[:,:,[0,-1]] = c_new[:,[1,-2]], c_new[:,:,[1,-2]]
         #set the corners, no flux through the corners
         c_new[:,[0,-1,0,-1],[0,-1,-1,0]] = c_new[:,[1,-2,1,-2],[1,-2,-2,1]] 
-        if blockpres:
-            for idx2 in range(lenblock):  # no flux through the wall of the blocking layer
-                c_new[:,blocked[idx2][0],blocked[idx2][1]] = c_new[:,block_surf[idx2][0],block_surf[idx2][1]]
-        
-        
-        
-        # rewrite this to one array instead of list with 4 arrays
-        
-        
-        
-        for idx2,el in enumerate(el_surf):
-            flux = (k_a[idx]*c_new[1,el[0],el[1]] - k_c[idx]*c_new[0,el[0],el[1]])/(1 + np.dot(diff[idx2]/D,k[:,idx]) ) # in mol/cm^s/s
-            bc = c_new[:,el[0],el[1]] + dD[idx2] * flux * normal[idx2] # normalized new value at the surface in mol/cm^3 
-            c_new[:,elec[idx2][0],elec[idx2][1]] = np.where(bc>0, bc, 0) # along y (x=0), prevents that c drops below 0
-       
-        
-       
+        if blockpres: c_new[:,blocked[0],blocked[1]] = c_new[:,blocked_surf[0],blocked_surf[1]]
+        # electrochemical part
+        flux_a = k_a[idx] * (A * c_new[1, el_surfx[0], el_surfx[1]] + B * c_new[1, el_surfy[0], el_surfy[1]]) # calculate the flux for the anodic reaction
+        flux_c = k_c[idx] * (A * c_new[0, el_surfx[0], el_surfx[1]] + B * c_new[0, el_surfy[0], el_surfy[1]]) # calculate the flux for the cathodic reaction
+        flux = (flux_a - flux_c)/(1 + C * (k_a[idx] + k_c[idx])) * np.array([[1],[-1]]) # change the sign the the order species: Jox = - Jred
+        bc = A * c_new[:,el_surfx[0], el_surfx[1]] + B * c_new[:,el_surfy[0], el_surfy[1]] + flux * C
+        c_new[:,elec[0],elec[1]] = np.where(bc > 0, bc, 0) # prevents that c drops below 0
         c_prev = c_new # overwrite previous value with new value
         if t == Time[counter_t]: # check if the output timestamp is reached, if yes store value
             c_cache[counter_t] = c_new
             counter_t += 1  
     #end of the sim 
-            
-    c_new = np.array(list(c_cache.values())) # extract the values from the directory
     
-    flux = np.ones((len(Time),1))
-    # current is determined by the flux of species A at the surface!
-    for idx,el in enumerate(el_surf):
-        first, last = np.where(el[0]==0)[0], np.where(el[0]==y_steps-1)[0]
-        el_surf[idx] = np.delete(el_surf[idx], np.append(first,last), axis=1)
-        elec[idx] = np.delete(elec[idx], np.append(first,last), axis=1)
-        newflux = - D[0]/diff[idx][0] *( c_new[:,0,el_surf[idx][0],el_surf[idx][1]] - c_new[:,0,elec[idx][0],elec[idx][1]] )
-        flux = np.append(flux, newflux, axis = 1)
-    flux = np.delete(flux, 0, axis=1) # remove the first colum, since we needed it for the correct shape
-    curdens = const.F * flux *1e3 # to mA/cm^2
+    # extract the values from the directory
+    c_new = np.array(list(c_cache.values())) 
     
+    # find the flux normal to the surface
+    flux_x = normal[1]/dx[0] * (c_new[:,0,el_surfx[0],el_surfx[1]] - c_new[:,0,elec[0],elec[1]])
+    flux_y = normal[0]/dy[0] * (c_new[:,0,el_surfy[0],el_surfy[1]] - c_new[:,0,elec[0],elec[1]])
+    flux = - D[0] * (flux_x + flux_y)
+    curdens = const.F * flux * 1e3 # to mA/cm^2
     
-    
-    
-    
-    
-    
-    # repeat for the x direction!!
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    # convert normalized vector back to [1,1] for corner elements
+    normal[tuple(np.where((normal > 0 ) & (normal < 1 )))] = 1
     
     # integrate the current density to obtain the line current density
-    L_elec, diff = np.array([]), np.delete([dy,dy,dx,dx],empty, axis=0)[:,0] # list with the lengths of the surface of the elements (so posxpos has a surface length of dy)
-    for idx,el in enumerate(elec): L_elec = np.append(L_elec, np.ones(len(el[0]))*diff[idx] )
+    L_elec = normal * np.array([dy[0],dx[0]]).reshape((2,1))
+    L_elec = np.sqrt(L_elec[0]**2+L_elec[1]**2)
     
     # devide the total line current by the total circumference of the electrode
     ave_curdens = np.sum(curdens*L_elec, axis=1) / np.sum(L_elec) # in mA/cm^2
     curdens = np.append(curdens, ave_curdens.reshape((len(curdens),1)), axis=1) # last element of curdens will be the average
+    
+    # remove the elements which are at the edge of the simulation area
+    # border : [0] = {0,ysteps-1}, [1] = {0,xsteps-1}
+    edge_y, edge_x = np.where((elec[1] == 0) | (elec[1] == x_steps-1)), np.where((elec[0] == 0) | (elec[0] == y_steps-1))
+    edge = np.concatenate((edge_x[0], edge_y[0]))
+    curdens = np.delete(curdens,edge,axis=1)
          
     if self.feedback: 
         tot_t = time.perf_counter() - t0
@@ -366,8 +306,7 @@ def CV_2D(self, *args, **kwargs):
         # k_a = ECrateconst(Eapp_r[idx-1], Ef0n, k0[1], T_C, -beta)
         # flux = (k_a*c_new[1,1] - k_c*c_new[0,1])/(1 + dx[1]/D[1]*k_a + dx[0]/D[0]*k_c ) # in mol/cm^s/s
     
-    self.time = Time
-    self.Eapp = Eapp
-    self.curdens = curdens
+    self.time, self.Eapp, self.curdens = Time, Eapp, curdens
     self.meshA, self.meshB = meshA, meshB
     self.cA, self.cB = c_new[:,0]*1e3, c_new[:,1]*1e3
+    

animate.py
@@ -6,8 +6,6 @@ Created on Wed Aug  5 11:21:20 2020
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import time
 import scipy.constants as const
 const.F = const.physical_constants["Faraday constant"][0]
 from .genfunc import Animate, Animate_2D
@@ -88,15 +86,31 @@ def animate_2D(self, *args, **kwargs):
     species = kwargs.get('species', 'A') # select which specices to animate
     if 'species' in kwargs: del kwargs['species']
     if 'units' in kwargs: del kwargs['units']
+    resolution = kwargs.get('resolution', 100)
+    if 'resolution' in kwargs: del kwargs['resolution']
     
     # select the c and mesh of the species:
-    if species == 'A': # select the mesh and c for species A
+    if species == 'A' or species == 'a': # select the mesh and c for species A
         mesh = self.meshA
         c = self.cA
-    elif species == 'B': # select the mesh and c for species B
+    elif species == 'B' or species == 'b': # select the mesh and c for species B
         mesh = self.meshB
         c = self.cB
     else: raise ValueError('Choose between the species \'A\' and \'B\' only!')
+    
+    elec = self.el # positions of the electrode
+    
+    # the mesh can be quite large sometimes, it is not always nessesary to display all pixels
+    if resolution < 100:
+        idx0 = np.round(np.linspace(0, mesh[0].shape[0] - 1, round(resolution/100 * mesh[0].shape[0]))).astype(int) # zeroth dim to shrink
+        idx1 = np.round(np.linspace(0, mesh[0].shape[1] - 1, round(resolution/100 * mesh[0].shape[1]))).astype(int) # first dim to shrink
+        mesh_LQ = mesh.copy()
+        mesh_LQ[0], mesh_LQ[1] = mesh_LQ[0][idx0][:,idx1], mesh_LQ[1][idx0][:,idx1]
+        c_LQ = c.copy()
+        c_LQ = c[:,idx0][:,:,idx1]
+        c, mesh = c_LQ, mesh_LQ
+        elec_LQ = np.array(np.round(elec.copy() * resolution/100-0.5)).astype(int) # shrink down the electrode pos
+        elec = np.unique(elec_LQ, axis=1) # remove all the duplicates
 
     f =[]
     # check which spatial units to use
@@ -131,7 +145,7 @@ def animate_2D(self, *args, **kwargs):
     if 'maxbar' not in kwargs: kwargs['maxbar'] = round(max(c.flatten()))
     if 'label' not in kwargs: kwargs['label'] = ['x ('+xunit+')','y ('+yunit+')']
     if 'barlabel' not in kwargs: kwargs['barlabel'] = 'Concentration ('+units[-1]+')'
-    if 'outline' not in kwargs: kwargs['outline'] =  self.el
+    if 'outline' not in kwargs: kwargs['outline'] =  elec
     
     # animate!
     anim = Animate_2D(x, c, **kwargs)
@@ -146,8 +160,8 @@ def animate_3D(self, x='all', y='all', z='all', *args, **kwargs):
     This function makes use of the animate_2D() function from GeneralFunctions.
     
     Parameters:
-        One of x, y or z to determine the cross section plane.
-        Otherwise see GeneralFunctions.animate_2D()
+        One of x, y or z to determine the cross section plane. (x=0 for example)
+        Otherwise see genfunc.animate_2D()
         
     Additional:
         species: specify which species to animate 'A' or 'B'
@@ -247,42 +261,50 @@ def animate_3D(self, x='all', y='all', z='all', *args, **kwargs):
     if 'ylim' not in kwargs: kwargs['ylim'] = [0, round(max(np.array(coord[1]).flatten()))]
     if 'minbar' not in kwargs: kwargs['minbar'] = 0
     if 'maxbar' not in kwargs: kwargs['maxbar'] = round(max(c.flatten()))
-    if 'label' not in kwargs: kwargs['label'] = ['x ('+xunit+')','y ('+yunit+')']
     if 'barlabel' not in kwargs: kwargs['barlabel'] = 'Concentration ('+units[-1]+')'
-    if 'outline' not in kwargs:
-        empty_inv = list(np.delete(range(6),self.empty))
-        outline = []
-        length = 0
+    
+    
+    ##### UPDATE THE OUTLINE 
+    # if 'outline' not in kwargs:
+    #     empty_inv = list(np.delete(range(6),self.empty))
+    #     outline = []
+    #     length = 0
         
-        if x !='all':
-            for i,el in enumerate(self.el):
-                if empty_inv[i] == 0 or empty_inv[i] == 1: continue
-                temp = el[:,el[2]==x]
-                pos = temp[(0,1),:]
-                outline.append(pos)
-                length = length + len(pos.flatten())
-            if length==0: print('The specified cross section does not include an electrode surface!')
-            else: kwargs['outline'] = outline
+    #     if x !='all':
+    #         for i,el in enumerate(self.el):
+    #             if empty_inv[i] == 0 or empty_inv[i] == 1: continue
+    #             temp = el[:,el[2]==x]
+    #             pos = temp[(0,1),:]
+    #             outline.append(pos)
+    #             length = length + len(pos.flatten())
+    #         if length==0: print('The specified cross section does not include an electrode surface!')
+    #         else: kwargs['outline'] = outline
             
-        if y !='all':
-            for i,el in enumerate(self.el):
-                if empty_inv[i] == 2 or empty_inv[i] == 3: continue
-                temp = el[:,el[1]==y]
-                pos = temp[(0,2),:]
-                outline.append(pos)
-                length = length + len(pos.flatten())
-            if length==0: print('The specified cross section does not include an electrode surface!')
-            else:kwargs['outline'] = outline
+    #     if y !='all':
+    #         for i,el in enumerate(self.el):
+    #             if empty_inv[i] == 2 or empty_inv[i] == 3: continue
+    #             temp = el[:,el[1]==y]
+    #             pos = temp[(0,2),:]
+    #             outline.append(pos)
+    #             length = length + len(pos.flatten())
+    #         if length==0: print('The specified cross section does not include an electrode surface!')
+    #         else:kwargs['outline'] = outline
             
-        if z !='all':
-            for i,el in enumerate(self.el):
-                if empty_inv[i] == 4 or empty_inv[i] == 5: continue
-                temp = el[:,el[0]==z]
-                pos = temp[(1,2),:]
-                outline.append(pos)
-                length = length + len(pos.flatten())
-            if length==0: print('The specified cross section does not include an electrode surface!')
-            else:kwargs['outline'] = outline
+    #     if z !='all':
+    #         for i,el in enumerate(self.el):
+    #             if empty_inv[i] == 4 or empty_inv[i] == 5: continue
+    #             temp = el[:,el[0]==z]
+    #             pos = temp[(1,2),:]
+    #             outline.append(pos)
+    #             length = length + len(pos.flatten())
+    #         if length==0: print('The specified cross section does not include an electrode surface!')
+    #         else:kwargs['outline'] = outline
+    
+    
+    if x != 'all' and'label' not in kwargs: kwargs['label'] = ['y ('+xunit+')','z ('+yunit+')']
+    elif y != 'all' and'label' not in kwargs: kwargs['label'] = ['x ('+xunit+')','z ('+yunit+')']
+    elif z != 'all' and 'label' not in kwargs: kwargs['label'] = ['x ('+xunit+')','y ('+yunit+')']
+    
     
     # animate!
     anim = Animate_2D(coord, c, **kwargs)

findsurface.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 """
 Created on Wed Aug  5 14:39:03 2020
 
@@ -11,22 +11,21 @@ from itertools import combinations
 def findsurface(self, electrode, retsurf=False):
     '''
     This function finds the surface of the electrode. It returns the
-    elements of the surface of the electrode, the elements which are the 
-    electrode, the normal vectors corresponding to the electrode elements,
-    and it returns the empty directions. When retsurf=True, it
-    returns an array with 0 for no electrode, 1 for electrode, and 2 for
-    the surface of the electrode. This is for displaying the surface.
+    elements of the surface of the electrode and the normal0 vectors corresponding
+    to the electrode elements. When retsurf=True, it returns an array with 0 
+    for no electrode, 1 for electrode, and 2 for the surface of the electrode.
+    This is for displaying the surface.
     
     Input parameters:
         dim: dimension, either '2D' or '3D'
         electrode: array containing 0 for no electrode and 1 for electrode (2D or 3D ndarray)
-        (optional) retsurf: set the surface of the electrode to the value 2 in the electrode array (default = False)
+        (optional) retsurf: set the surfgace of the electrode to the value 2 in the electrode array (default = False)
     
     Output parameters:
-        el_surf: list with the positions of the surface of the electrode in the electrolyte (1D ndarray)
-        elec: list with the positions of the surface of the electrode (1D ndarray)
-        empty: list with directions which does not contain an electrode (1D list)
-        (optional) electrode_surf: array with 0 if electrolyte, 1 if electrode, 2 if surface (2D or 3D ndarray)
+        elec0: list with the positions of the surface of the electrode (2D ndarray)
+        normal0: normal0 vector to the surface of the electrode (2D ndarray)
+        (optional) electrode_surf: array with 0 if electrolyte, 1 if electrode, 
+                   2 if surface, 3 if corner element (2D or 3D ndarray)
     '''
     if self.dim == '2D':
         posxpos, posxneg = [], []
@@ -67,27 +66,56 @@ def findsurface(self, electrode, retsurf=False):
         # specify the surface elements
         displace = np.array([[0,0,-1,1],[-1,1,0,0]]).T.reshape((4,2,1))
         displace = np.delete(displace, empty, axis=0)
-        elec = [el_surf[i] + displace[i] for i in range(len(el_surf))] # list with electrode positions
+        elec0 = [el_surf[i] + displace[i] for i in range(len(el_surf))] # list with electrode positions
         
         # find the normal to the surface
-        normalvec = np.delete(np.array([[[1,0]],[[1,0]],[[0,1]],[[0,1]]]), empty, axis=0)
-        cornerel, normal = np.array([[],[]]), []
-        for i, el in enumerate(elec): # search within elec for corner elem
+        normalvec = np.delete(np.array([[[0,1]],[[0,-1]],[[1,0]],[[-1,0,]]], dtype=int), empty, axis=0) # dy, dx
+        cornerel, normal0 = np.array([[],[]]), []
+        for i, el in enumerate(elec0): # search within elec0 for corner elem
             cornerel = np.concatenate((cornerel, el), axis=1)
-            normal.append(el*0.0+normalvec[i].T)
+            normal0.append(el*0+normalvec[i].T)
         uniq, count = np.unique(cornerel.T, return_counts=True, axis=0)
         cornerel = uniq[count > 1].T.astype(np.int) # here the positions of the corner elements in the 2D matrix are being saved 
+         
+        # merge elec and normal to one 1D ndarray
+        elec, normal =  elec0[0], normal0[0]
+        for i in np.arange(1,len(elec0)):
+            elec = np.concatenate((elec, elec0[i]), axis=1)
+            normal = np.concatenate((normal, normal0[i]), axis=1)    
+            
+        # sort the elec and normal arrays
+        elec, normal = elec.T, normal.T
+        for i in [1,0]:
+            sortel = elec[:,i].argsort(kind='mergesort')
+            elec, normal = elec[sortel], normal[sortel]
+        
+        # there are 4 situations, the elements of the surf can border to:
+        # 1 element:  just x or y dir
+        # 2 elements: corner element, flux will be the sum of the two corrected by the normal0 vector
+        # 3 elements: only the unique direction should survive, so (1,0) + (0,1) + (-1,0) sums up to (0,1)
+        # 4 elements: no direction survices, there is no electrode...       
+        uniq, idx_uniq, count = np.unique(elec, axis=0, return_counts=True, return_index=True)
+        dup = np.where(count>1)[0] # select only the duplicates
+        for el, cnt in zip(uniq[dup],count[dup]): # find all elements which are duplicates
+            find = np.intersect1d(np.where(elec[:,0]==el[0]), np.where(elec[:,1]==el[1]))
+            normal[find] = np.sum(normal[find],axis=0) # sum up their normal vectors
+            
+        # select only the unique elements
+        elec, normal = uniq.T, normal[idx_uniq].T
         
         if retsurf:
             electrode_surf = electrode.copy()
             for el in el_surf: electrode_surf[tuple(el)] = 2 # give the electrode surface a different value
             # plot corner elements
             electrode_surf[tuple(cornerel)] = 3 # give the electrode corners a different value
-            
-            return el_surf, elec, empty, normal, electrode_surf
-        
+            return elec, normal, electrode_surf
         else:
-            return el_surf, elec, empty, normal
+            return elec, normal
+        
+        
+        # empty can be removed in the future
+        
+        # check if elec0 and el_surf can be removed -> blocking layer/activity
      
         
      
@@ -147,54 +175,54 @@ def findsurface(self, electrode, retsurf=False):
         # specify the surface elements
         displace = np.array([[0,0,0,0,-1,1],[0,0,-1,1,0,0],[-1,1,0,0,0,0]]).T.reshape((6,3,1))
         displace = np.delete(displace, empty, axis=0)
-        elec = [el_surf[i] + displace[i] for i in range(len(el_surf))] # list with electrode positions
+        elec0 = [el_surf[i] + displace[i] for i in range(len(el_surf))] # list with electrode positions
         
         # find the normal to the surface
-        normalvec = np.delete(np.array([[[1,0,0]],[[1,0,0]],[[0,1,0]],[[0,1,0]],[[0,0,1]],[[0,0,1]]]), empty, axis=0)
-        normal = []
-        for i, el in enumerate(elec):
-            normal.append(el*0.0+normalvec[i].T)
+        normalvec = np.delete(np.array([[[1,0,0]],[[-1,0,0]],[[0,1,0]],[[0,-1,0]],[[0,0,1]],[[0,0,-1]]]), empty, axis=0)
+        normalvec = np.delete(np.array([[[0,0,1]],[[0,0,-1]],[[0,1,0]],[[0,-1,0]],[[1,0,0]],[[-1,0,0]]]), empty, axis=0)
+        normal0 = []
+        for i, el in enumerate(elec0): normal0.append(el*0.0+normalvec[i].T)
         
-        if len(normalvec)>1: # edges and corners are only present for more than one surface direction
-                            
-            # assign the correct normal vectors to the edge elements
-            empty_inv = list(np.delete(range(6),empty))
-            comb_indices = list(combinations(empty_inv,2))
-            normalvec_edge = np.array(list(combinations(normalvec,2)))
-            normalvec_edge = np.sum(normalvec_edge,axis=1)
-            edges = np.array([[],[],[]])
-            for i,comb in enumerate(list(combinations(elec,2))):
-                if comb_indices[i] in ((0,1),(2,3),(4,5)): continue # skip combinations of the same coordinate
-                duplicates_temp = np.concatenate(comb,axis=1)
-                uniq_temp, count_temp = np.unique(duplicates_temp.T, return_counts=True, axis=0)
-                edges_temp = uniq_temp[count_temp == 2].T.astype(np.int)
-                edges = np.concatenate((edges,edges_temp), axis=1)
-                a0, b0, c0 = np.in1d(comb[0][0], edges_temp[0]), np.in1d(comb[0][1], edges_temp[1]), np.in1d(comb[0][2], edges_temp[2])
-                a1, b1, c1 = np.in1d(comb[1][0], edges_temp[0]), np.in1d(comb[1][1], edges_temp[1]), np.in1d(comb[1][2], edges_temp[2])
-                for j in np.where(a0*b0*c0)[0]: normal[comb_indices[i][0]][:,j] = normalvec_edge[i][0]
-                for j in np.where(a1*b1*c1)[0]: normal[comb_indices[i][1]][:,j] = normalvec_edge[i][0]
-                
-            duplicates = np.array([[],[],[]])
-            for i, el in enumerate(elec): # search within elec for duplicates
-                duplicates = np.concatenate((duplicates, el), axis=1)
-            uniq, count = np.unique(duplicates.T, return_counts=True, axis=0)
+        # merge elec and normal to one 1D ndarray
+        elec, normal = elec0[0], normal0[0]
+        for i in np.arange(1,len(elec0)):
+            elec = np.concatenate((elec, elec0[i]),axis=1)
+            normal = np.concatenate((normal, normal0[i]), axis=1)
             
-            # here the normal vector [1,1,1] is assigned to each corner element of the 3D matrix
-            corners = uniq[count == 3].T.astype(np.int)
-            for i,el in enumerate(elec):
-                a, b, c = np.in1d(el[0], corners[0]), np.in1d(el[1], corners[1]), np.in1d(el[2], corners[2])
-                for j in np.where(a*b*c)[0]: normal[i][:,j] = np.ones(3)
+        # sort the elec and normal arrays
+        elec, normal = elec.T, normal.T
+        for i in [2,1,0]:
+            sortel = elec[:,i].argsort(kind='mergesort')
+            elec, normal = elec[sortel], normal[sortel]
             
+        # there are 6 situations, the elements of the surf can border to:
+        # 1 element:  just x, y or z dir
+        # 2 elements: 2D edge element, flux will be the sum of the two corrected by the normal0 vector
+        # 3 elements: 3D corner element
+        # 4 elements: Corner of a slab: (1,0,0) + (-1,0,0) + (0,1,0) + (0,0,1) = (0,1,1)       
+        # 5 elements: Top of a rod, only one dir should survive
+        # 6 elements: this piece is not connected to the rest of the electrode, no direction should survive: (0,0,0)
+        uniq, idx_uniq, count = np.unique(elec, axis=0, return_counts=True, return_index=True)
+        dup = np.where(count>1)[0] # select only the duplicates
+        for el, cnt in zip(uniq[dup],count[dup]): # find all elements which are duplicates
+            find = np.intersect1d(np.intersect1d(np.where(elec[:,0]==el[0]), np.where(elec[:,1]==el[1])), np.where(elec[:,2]==el[2]))
+            normal[find] = np.sum(normal[find],axis=0) # sum up their normal vectors
+            
+        # select only the unique elements
+        elec, normal = uniq.T, normal[idx_uniq].T
+        
         if retsurf:
             electrode_surf = electrode.copy()
             for el in el_surf: electrode_surf[tuple(el)] = 2 # give the electrode surface a different value
             if len(normalvec)>1:
+                edges = uniq[count == 2].T.astype(np.int)
+                corners = uniq[count > 2].T.astype(np.int)
                 electrode_surf[tuple(edges)] = 3 # give the electrode edges a different value
                 electrode_surf[tuple(corners)] = 4 # give the electrode corners a different value
             
-            return el_surf, elec, empty, normal, electrode_surf
+            return elec, normal, electrode_surf
         
         else:
-            return el_surf, elec, empty, normal
+            return elec, normal, el_surf, elec0, empty, normal0
         
     else: raise ValueError('dim should be \'2D\' or \'3D\'!')    

genfunc.py
@@ -432,8 +432,6 @@ def Animate_2D(mesh2D, z, *args, **kwargs):
             plotted in the graph)
         outline: draws an outline. (2D np.array)
             [[xel1, xel2, ...],[yel1, yel2, ...]]
-            or
-            [[xel1, xel2, ...],[yel1, yel2, ...] , [xel10, xel11, ...],[yel10, yel11, ...], ...]
         outlinecolor: the color of the outline
         
     Output: 
@@ -518,11 +516,7 @@ def Animate_2D(mesh2D, z, *args, **kwargs):
     fig, ax = fancyplot(**options)
     quad = ax.pcolormesh(X, Y, z[0], **options2D)
     if outline is not None:
-        x_outline = np.array([])
-        y_outline = np.array([])
-        for el in outline:
-            x_outline = np.append(x_outline, X[tuple(el)])
-            y_outline = np.append(y_outline, Y[tuple(el)])
+        x_outline,  y_outline = X[tuple(outline)], Y[tuple(outline)]
         plt.scatter(x_outline,y_outline, marker='s', s=2, color=outlinecolor)
     line, = ax.plot([],[])
     bar = fig.colorbar(quad)

plot.py
@@ -41,8 +41,8 @@ def plot(self, Type='CV', *args, **kwargs):
     retfig = kwargs.get('returnfig', False)
     
     if not self.runned: raise Warning('Run the simulation first using .run()!')
-    if Type not in ['CV', 'tE', 'tI', 'tcA', 'tcB', 'tc']:
-        raise ValueError('Type should be one of the following: \'CV\', \'tE\', \'tI\', \'tcA\', \'tcB\' or \'tc\'!')
+    if Type not in ['CV', 'CV all', 'tE', 'tI', 'tcA', 'tcB', 'tc', 'te', 'ti', 'tca', 'tcb']:
+        raise ValueError('Type should be one of the following: \'CV\', \'CV all\', \'tE\', \'tI\', \'tcA\', \'tcB\' or \'tc\'!')
     
     # copy the kwargs and remove the keys that are not used in the plot
     plotoptions = kwargs.copy()
@@ -50,7 +50,7 @@ def plot(self, Type='CV', *args, **kwargs):
     except KeyError: pass
 
     # check the units
-    if Type == 'CV' or Type == 'tI':
+    if Type == 'CV' or Type == 'CV all' or Type == 'tI' or Type == 'ti':
         units = kwargs.get('units', 'mA/cm2')
         if units == 'mA/cm2':
             units = '(mA/cm$^2$)'
@@ -63,7 +63,7 @@ def plot(self, Type='CV', *args, **kwargs):
             curdens = 10*self.curdens
         else:
             raise ValueError('The units should be \'mA/cm2\', \'mA/m2\' or \'A/m2\'!')
-    elif Type == 'tcA' or Type == 'tcB' or Type == 'tc':
+    elif Type == 'tcA' or Type == 'tcB' or Type == 'tca' or Type == 'tcb' or Type == 'tc':
         units = kwargs.get('units', 'mM')
         if units == 'mM':
             cA = 1e3*self.cA
@@ -85,20 +85,39 @@ def plot(self, Type='CV', *args, **kwargs):
     checkkeys('plot()', kwargs, allowedkeys)
 
     if Type == 'CV':
+        # this is for ploting the current versus the potential
+        # only the average current density will be shown, this is the closest the the experiment
         plotoptions['label'] = ['Potential (V)', 'Current density '+units]
         if self.dim == '1D': 
             fig = fancyplot(self.Eapp, curdens, **plotoptions)
         elif self.dim == '2D' or self.dim=='3D':
             plotoptions['title'] = 'Average current density'
+            plotoptions['color'] = 'r' 
             fig = fancyplot(self.Eapp, curdens[:,-1], **plotoptions)
             
+    if Type == 'CV all':
+        # plotting all CV's of all elements together with its averaged value
+        plotoptions['label'] = ['Potential (V)', 'Current density '+units]
+        if self.dim == '1D': 
+            fig = fancyplot(self.Eapp, curdens, **plotoptions)
+        elif self.dim == '2D' or self.dim=='3D':
+            plotoptions['title'] = 'Average current density'
+            plotoptions['color'] = 'r'
+            plotoptions['showfig'] = False
+            fig = fancyplot(**plotoptions)
+            plt.plot(self.Eapp, curdens[:,:-1], 'gray')
+            plt.plot(self.Eapp, curdens[:,-1], 'r')
+            plt.show()
             
-    elif Type == 'tE':
+            
+    elif Type == 'tE' or Type == 'te':
+        # plot the time vs potential
         plotoptions['label'] = ['Time (s)', 'Potential (V)']
         fig = fancyplot(self.time, self.Eapp, **plotoptions)
         
         
-    elif Type == 'tI':
+    elif Type == 'tI' or Type == 'ti':
+        # plot the average current density vs time
         plotoptions['label'] = ['Time (s)', 'Current density '+units]
         if self.dim == '1D': 
             fig = fancyplot(self.time, curdens, **plotoptions)
@@ -107,7 +126,8 @@ def plot(self, Type='CV', *args, **kwargs):
             fig = fancyplot(self.time, curdens[:,-1], **plotoptions)
             
             
-    elif Type == 'tcA':
+    elif Type == 'tcA' or Type == 'tca':
+        # plot the concentration of A vs "the distance from the electrode"
         try: plotoptions['title'] # check if title is specified
         except KeyError: plotoptions['title'] = 'Surface concentration vs time'
         if self.dim == '1D':
@@ -116,9 +136,12 @@ def plot(self, Type='CV', *args, **kwargs):
             
             # update this!
             fig = fancyplot(self.time, cA[:,50,0], **plotoptions)
+
+        elif self.dim == '3D':
+            raise NotImplementedError('3D plot not implemented yet!')            
             
-            
-    elif Type == 'tcB':
+    elif Type == 'tcB' or Type == 'tcb':
+        # plot the concentration of B vs "the distance from the electrode"
         try: plotoptions['title'] # check if title is specified
         except KeyError: plotoptions['title'] = 'Surface concentration vs time'
         if self.dim == '1D':
@@ -127,9 +150,12 @@ def plot(self, Type='CV', *args, **kwargs):
             
             # update this!
             fig = fancyplot(self.time, cB[:,50,0], **plotoptions)
-            
+        
+        elif self.dim == '3D':
+            raise NotImplementedError('3D plot not implemented yet!')     
             
     elif Type == 'tc':
+        # plot both the concentrations of A and B vs the "distance from the electrode"
         try: plotoptions['legend'] # check if legend is specified
         except KeyError: plotoptions['legend'] = ['A','B']
         try: plotoptions['title'] # check if title is specified
@@ -159,7 +185,7 @@ def activity(self, tstart=None, tend=None, units=['um','C/cm2'], showhis=False,
         units: specify the units of the labels
     
     '''
-    if self.dim != '2D': raise ValueError('This function is only available for 2D!')
+    if self.dim != '2D': raise NotImplementedError('This function is only available for 2D!')
     
     if tstart is None: tstart = 0
     else: tstart = find_nearest(self.time, tstart)
@@ -171,11 +197,17 @@ def activity(self, tstart=None, tend=None, units=['um','C/cm2'], showhis=False,
     curdens = self.curdens[tstart:tend,:-1]
     chargedens = abs(np.trapz(curdens, t, axis=0))
 
-    # set the charge transfer grid
+    # set the charge transfer grid, use the size of the electrode
     chargedensgrid = self.elec*0
-    elements = np.array([[],[]])
-    for el in self.el: elements = np.append(elements, el,axis=1)
-    chargedensgrid[tuple(elements.astype(int))] = chargedens
+
+    # remove the corner elements from elec
+    elec = self.el
+    edge_y, edge_x = np.where((elec[0] == 0) | (elec[0] == self.y_steps-1)), np.where((elec[1] == 0) | (elec[1] == self.x_steps-1))
+    edge = np.concatenate((edge_x[0], edge_y[0]))
+    elec = np.delete(elec,edge,axis=1)
+
+    # set the values of the grid
+    chargedensgrid[tuple(elec)] = chargedens
     
     f =[]
     # check which spatial units to use